65bdfc0d4fe934cc57caa3accf9bbfaff2033984
Daniel Salinetti <dansalinetti@gmail.com>
2017-08-23 11:42:53 -0400

    Still debugging redirct to thanks page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-console.log('hello')
 // Hidden email for formspree.io
 var contactform =  document.getElementById('contactform');
 contactform.setAttribute('action', '//formspree.io/' + 'dansalinetti' + '@' + 'gmail' + '.' + 'com');
