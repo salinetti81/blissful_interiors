c7e3c44a85a4b504dc8a7f4185ea22c9d23024c8
Daniel Salinetti <dansalinetti@gmail.com>
2017-08-23 08:08:27 -0400

    Updating email adress for contact form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Hidden email for formspree.io
 var contactform =  document.getElementById('contactform');
-contactform.setAttribute('action', '//formspree.io/' + 'dansalinetti' + '@' + 'gmail' + '.' + 'com');
+contactform.setAttribute('action', '//formspree.io/' + 'carolyn' + '@' + 'blissfulinteriordesign' + '.' + 'com');
 
 
 // Interiors Slideshow
