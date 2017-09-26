1d89b3c2e069940987012a927bd0d8e6c140795d
Daniel Salinetti <dansalinetti@gmail.com>
2017-09-26 14:37:24 -0400

    Adding jquery slide up function on about page for creative process

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+
+
+// About Page slide up
+$(document).ready(function() {
+  $("#creativeSlideUp").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#creativeProcess").offset().top -70},
+        'slow');
+  });
+});//ends document.ready for jQuery
+
 // Hidden email for formspree.io
 var contactform =  document.getElementById('contactform');
 contactform.setAttribute('action', '//formspree.io/' + 'dansalinetti' + '@' + 'gmail' + '.' + 'com');
-
-
-// Interiors Slideshow
-
